211a983090dfc358f3776be8ec037e2161fc0896
Monika Stempniak <monika.stempniak@accenture.com>
2020-06-28 18:39:00 +0200

    Create express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/build"));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const server = app.listen(8081, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+});
